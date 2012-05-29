3f5368d174f42f784ed0261edd069023e70c4522
Robert Chang <rchang@fulcrum.net>
2012-05-29 14:53:23 -0700

    Initial commit

---
+++ lib/prime_finder.rb
@@ -0,0 +1,48 @@
+#
+# Uses the Sieve of Eratosthenes algorithm to find the prime numbers given an upper bound
+#
+class PrimeFinder
+
+  def find_prime (upper_bound)
+
+    sqrt_upper = (Math.sqrt upper_bound).round
+    working_arr = Array.new(upper_bound) {|e| e =  false}
+    return_arr = Array[]
+
+    for m in 2 .. sqrt_upper
+      if !working_arr[m]
+        return_arr << m
+        k = m * m
+        while k <= upper_bound
+          working_arr[k] = true
+          k += m
+        end
+      end
+    end
+
+    for n in sqrt_upper+1 .. upper_bound
+      if !working_arr[n]
+        return_arr << n
+      end
+    end
+
+    return_arr
+
+  end
+
+  ########
+  # Main #
+  ########
+
+  finder = PrimeFinder.new
+
+  if ARGV.size == 0
+    puts ("USAGE: ruby prime.rb <upper bound>")
+    puts ("EXAMPLE: ruby prime.rb 30")
+  else
+    if ARGV.size == 1 && ARGV[0].to_i != 0
+      upper_bound = ARGV[0].to_i
+      puts finder.find_prime(upper_bound)
+    end
+  end
+end
---
+++ spec/prime_finder_spec.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+require '../lib/prime_finder'
+
+describe "Test" do
+  it "should find prime with the upper bound given" do
+    upper_bound = 10
+    return_arr = Array[2,3,5,7,9]
+    finder = PrimeFinder.new
+    finder.should_receive(:find_prime).with(upper_bound).and_return(return_arr)
+    finder.find_prime(upper_bound)
+  end
+
+  it "should return an array of prime numbers from find_prime" do
+    upper_bound = 10
+    return_arr = Array[2,3,5,7,9]
+    finder = PrimeFinder.new
+    finder.stub(:find_prime => return_arr)
+    finder.find_prime(upper_bound).should eql return_arr
+  end
+
+  it "should return an array as the result from find_prime" do
+    upper_bound = 10
+    finder = PrimeFinder.new
+    finder.find_prime(upper_bound).class.should eql Array
+  end
+
+  it "should return an array with the correct prime numbers from find_prime" do
+    upper_bound = 10
+    finder = PrimeFinder.new
+    return_arr = finder.find_prime(upper_bound)
+    return_arr.size.should eql 4
+    return_arr[0].should eql 2
+    return_arr[1].should eql 3
+    return_arr[2].should eql 5
+    return_arr[3].should eql 7
+  end
+
+  it "should return an empty array if upper bound 0 is passed into find_prime" do
+    upper_bound = 0
+    finder = PrimeFinder.new
+    return_arr = finder.find_prime(upper_bound)
+    return_arr.size.should eql 0
+  end
+
+end
